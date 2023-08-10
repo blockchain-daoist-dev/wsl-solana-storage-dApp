b1ccc1cc8c8c668acf293704ff4cca7680caf206
daoistcoder <jeromeballena.dev@gmail.com>
2023-08-11 03:06:07 +0800

    UPDATE: app.js

--- frontend/myproject/src/App.js
+++ frontend/myproject/src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
 function App() {
+  const [walletAddress, setWalletAddres] = React.useState(null);
+
+  window.onload = async function () {
+    try {
+      if (window.solana) {
+
+        const solana = window.solana;
+        if (solana.isPhantom) {
+          alert('Phantom wallet found!');
+          const res = await solana.connect({ onlyIfTrusted: true })
+          console.log('connected with publicKey:', res.publicKey.toString());
+        }
+      } else {
+        alert('wallet not found!')
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        {!walletAddress && (
+          <div>
+            <button className='btn' onClick={connectWallet}>
+              Connect Wallet
+            </button>
+          </div>
+        )}
+        {walletAddress && (
+          <div>
+            <p>
+              Connected Account : {''}
+              <span className='adddress' {walletAddress}></span>
+            </p>
+          </div>
+        )}
       </header>
     </div>
   );
